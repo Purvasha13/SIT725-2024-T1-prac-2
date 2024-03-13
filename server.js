908ac3fb8bfe3cc7760cb37c501f9aff9d1b9d77
Purvasha <purvasha1013@gmail.com>
2024-03-13 17:04:42 +1100

    2.2

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ app.get('/addnumbers', (req, res) => {
     var { num1, num2 } = req.query;
     var sum = Number(num1) + Number(num2);
     
-    if (!isNaN(Number(num1)) && !isNaN(Number(num2))) {
+    if ((num1) && (num2) != null) {
         res.status(200).json({ data: sum, message: "success",statuscode: 200 });
     } else {
         res.status(400).json({ data: "null",message: "Invalid numbers provided",statuscode: 400 });
-    }
-});
+    }});
 
 app.listen(PORT, () => 
 console.log(`Server running on port ${PORT}`));
