89a60cc26059b5742339150faffcce3a5c905bb8
Purvasha <purvasha1013@gmail.com>
2024-03-15 01:03:03 +1100

    2.2

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.get('/addnumbers', (req, res) => {
         res.status(400).json({ data: "null",message: "Invalid numbers provided",statuscode: 400 });
     }});
 
+app.get('/subnumbers', (req, res) => {
+    var { num1, num2 } = req.query;
+    var sub = Number(num1) - Number(num2);
+        
+    if ((num1) && (num2) != null) {
+         res.status(200).json({ data: sub, message: "success",statuscode: 200 });
+    } else {
+        res.status(400).json({ data: "null",message: "Invalid numbers provided",statuscode: 400 });
+    }});
+
 app.listen(PORT, () => 
 console.log(`Server running on port ${PORT}`));
 
